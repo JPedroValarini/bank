a204e2ba8ef9dce557a5a16755a45216acf0b6d3
joao <joao@moat.ai>
2023-05-05 17:25:00 -0300

    adjust to render site

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,5 +6,4 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:create
-bundle exec rake generate migration add_password_digest_to_users password_digest:string
 bundle exec rake db:migrate
